2ee1bdb46634971492c2c4fbe98c675c4b33cdb5
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-29 08:12:31 -0500

    Updated from Brython Server: 1/29/2018 8:12:30 AM

--- ifDemo.py
+++ ifDemo.py
@@ -5,8 +5,11 @@
 num = float(input('Enter a number: '))
 
 if num > 0:
-    print(num, 'is positive')
-elif:
+    print(num,' is positive')
+elif num < 0:
     print(num, 'is negative')
+else:
+    print(num, 'is zero')
+    
     
     
