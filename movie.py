8deb9d1940ecc8d46dc17de06fe80b9b02a6ae9d
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-29 08:28:07 -0500

    Updated from Brython Server: 1/29/2018 8:28:06 AM

--- movie.py
+++ movie.py
@@ -1,1 +1,12 @@
+#Christo Dragnev
+#1/29/18
+#movie.py
 
+age = float(input('Enter your age: '))
+
+if age >= 18:
+    print('You can watch R movies')
+elif age>=13:
+    print('You can watch PG-13 movies')
+else:
+    print('You can watch PG movies')
