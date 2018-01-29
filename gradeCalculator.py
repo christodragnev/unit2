76544c4e116ae62fa0ba290649779c7daa74475b
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-29 08:24:20 -0500

    Updated from Brython Server: 1/29/2018 8:24:20 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,18 @@
+#Christo Dragnev
+#1/29/18
+#gradeCalculator.py
+
+grade = float(input('Your grade: '))
+
+if grade >= 90:                           
+    print('You earned an A')
+elif grade >= 80:
+    print('You earned an B')
+elif grade >= 70:
+    print('You earned an C')
+elif grade >= 60:
+    print('You earned an D')
+else:
+    print('You earned an NC')
+
 
