4022c0be9b563256a6154b4ea11c8eb443eb9c39
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-30 08:33:22 -0500

    Updated from Brython Server: 1/30/2018 8:33:21 AM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,23 @@
+#Christo Dragnev
+#1/30/18
+#insulter.py
+
+from random import randint
+
+name = str(input('Enter your name: '))
+
+num = randint(1,5)
+
+if num==1:
+    print('My program is smarter than you')
+elif num==2:
+    print('My grandmother can run faster you')
+elif num==3:
+    print('You are dumb')
+elif num==4:
+    print('Get good')
+elif num==5:
+    print('You are the opposite of cool')
+    
+
 
