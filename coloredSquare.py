03496d26cf7f6a8cd792110e19f77460a2fa0980
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-01-31 10:31:30 -0500

    Updated from Brython Server: 1/31/2018 10:31:29 AM

--- coloredSquare.py
+++ coloredSquare.py
@@ -13,7 +13,7 @@ yellow = 4
 num = randint(1,4)
 
 if num == 1:
-    print(red = Color(0xff0000,1))
+    red = Color(0xff0000,1)
     line = LineStyle(3,red)
     rectangle1 = RectangleAsset(100,100,line,red)
 
@@ -21,7 +21,7 @@ if num == 1:
     myApp = App()
     myApp.run()
 elif num ==2:
-    print(blue = Color(0x0000ff,1))
+    blue = Color(0x0000ff,1)
     line = LineStyle(3,blue)
     rectangle2 = RectangleAsset(100,100,line,blue)
 
@@ -29,18 +29,20 @@ elif num ==2:
     myApp = App()
     myApp.run()
 
-green = Color(0x00cc00,1)
-line = LineStyle(3,green)
-rectangle3 = RectangleAsset(100,100,line,green)
+elif num==3:
+    green = Color(0x00cc00,1)
+    line = LineStyle(3,green)
+    rectangle3 = RectangleAsset(100,100,line,green)
 
-Sprite(rectangle3)
-myApp = App()
-myApp.run()
+    Sprite(rectangle3)
+    myApp = App()
+    myApp.run()
 
-yellow = Color(0xffff00,1)
-line = LineStyle(3,yellow)
-rectangle4 = RectangleAsset(100,100,line,yellow)
+elif num==4:
+    yellow = Color(0xffff00,1)
+    line = LineStyle(3,yellow)
+    rectangle4 = RectangleAsset(100,100,line,yellow)
 
-Sprite(rectangle4)
-myApp = App()
-myApp.run()
+    Sprite(rectangle4)
+    myApp = App()
+    myApp.run()
