aa12994b45fd9f3ffa22f7f4d58c175a2dfb3d95
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-02-05 08:40:51 -0500

    Create slope.py

---
+++ slope.py
@@ -0,0 +1,13 @@
+#Christo
+#2/2/18
+#slope.py
+
+x1 = float(input('Enter x1: '))
+y1 = float(input('Enter y1: '))
+x2 = float(input('Enter x2: '))
+y2 = float(input('Enter y2: '))
+
+if x2 - x1 == 0:
+    print('The slope is undefined')
+else: 
+    print('The slope is = (y2 - y1)/(x2 - x1)
