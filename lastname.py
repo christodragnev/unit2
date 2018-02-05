f9bd3cee7eb27dd2776b5558e9fde7169e140b6a
christodragnev <35492777+christodragnev@users.noreply.github.com>
2018-02-05 08:41:28 -0500

    Create lastname.py

---
+++ lastname.py
@@ -0,0 +1,11 @@
+#Christo Dragnev
+#2/2/18
+#lastname.py
+
+name = input('Enter your lastname: ')
+
+
+if name<'M':
+    print(name, "is in the first half of the alphabet")
+else:
+    print(name, "is in the second half of the alphabet")
